1b619f21c6765b7d8c2fc97c7db59617f5564cd9
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2022-05-21 19:38:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,10 +407,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/DTG_BOTS')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/FULL_OF_ENTERTAINMENT')
         ], [
             InlineKeyboardButton('🌟 Main Channel', url='https://t.me/DTG_TV'),
-            InlineKeyboardButton('💥 Bot Movie Channel', url='https://t.me/yetflix')
+            InlineKeyboardButton('💥 Bot Movie Channel', url='https://t.me/+JvBQVB9fa4ExMmFl')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -441,7 +441,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria'),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/FULL_OF_ENTERTAINMENT'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
