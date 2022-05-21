c97bdfb3ed3b8fc05245b08b4ba31663a5065db3
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2022-05-21 20:45:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 🌟 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 🌟 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- COMING SOON 
+
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
