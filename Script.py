9a388b1c7498987977e8ba66313f28e9e93eaa64
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2022-05-21 20:47:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,6 +12,10 @@ class script(object):
 🌟 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 
+
+
+
+
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
